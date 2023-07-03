21fb5469ed20f0672f04951d05577bc0554c0823
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-03 21:09:59 +0300

    re-exports components paths

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+// re-exports components paths here
+
+// ----- common -----
+export { default as Header } from './Common/Header';
+export { default as Footer } from './Common/Footer/Footer';
+
+// ----- UI -----
+export { default as Card } from './UI/Card/Card';
+export { default as Button } from './UI/Button/Button';
+export { default as Logo } from './UI/Logo';
+
+// ---- Car Hub -----
+export { default as CarHub } from './CarHub/CarHub';
+
+// ---- Hero -----
+export { default as Hero } from './Hero/Hero';
+export { default as HeroSummary } from './Hero/HeroSummary';
+export { default as HeroImage } from './Hero/HeroImage';
