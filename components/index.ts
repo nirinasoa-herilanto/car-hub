4cc4b4f1e2bfc374175ed95d326bb9b06058c515
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-04 18:10:25 +0300

    feat: add car components
    contains:
    - Carlists component, use to display all cars data
    - CarItem component, use to display a specific car data

--- components/index.ts
+++ components/index.ts
@@ -8,6 +8,7 @@ export { default as Footer } from './Common/Footer/Footer';
 export { default as Card } from './UI/Card/Card';
 export { default as Button } from './UI/Button/Button';
 export { default as Logo } from './UI/Logo';
+export { default as Loading } from './UI/Loading';
 
 // ---- Car Hub -----
 export { default as CarHub } from './CarHub/CarHub';
@@ -16,3 +17,7 @@ export { default as CarHub } from './CarHub/CarHub';
 export { default as Hero } from './Hero/Hero';
 export { default as HeroSummary } from './Hero/HeroSummary';
 export { default as HeroImage } from './Hero/HeroImage';
+
+// ---- Car ------
+export { default as CarItem } from './Car/CarItem';
+export { default as CarLists } from './Car/CarLists';
