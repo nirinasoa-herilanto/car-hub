f149c877cd17ee0383c0d3e5643a311f9003e9c0
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-07-06 19:04:30 +0300

    update: add limit results variables

--- config/app.config.ts
+++ config/app.config.ts
@@ -5,4 +5,5 @@ export const carHubConfig = {
   carsApiEndpoint: process.env.NEXT_PUBLIC_CARS_API_ENDPOINT,
   rapidApiKey: process.env.NEXT_PUBLIC_RAPID_API_KEY,
   rapidApiHost: process.env.NEXT_PUBLIC_RAPID_API_HOST,
+  limitResults: 9,
 };
